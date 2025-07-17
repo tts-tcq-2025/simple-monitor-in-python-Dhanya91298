c273a194c63b064722ab378cc8ce23f480da8a95
Dhanya91298 <Ramadas.Dhanya@in.bosch.com>
2025-07-17 11:05:23 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,15 +1,22 @@
+def tempCheck(temperature)
+    if temperature < 0 or temperature > 45:
+      print('Temperature is out of range!')
+      return False
+
+def socCheck(soc)
+    if soc < 20 or soc > 80:
+      print('State of Charge is out of range!')
+      return False
+
+def chargeCheck(charge_rate)
+    if charge_rate > 0.8:
+      print('Charge rate is out of range!')
+      return False
 
 def battery_is_ok(temperature, soc, charge_rate):
-  if temperature < 0 or temperature > 45:
-    print('Temperature is out of range!')
-    return False
-  elif soc < 20 or soc > 80:
-    print('State of Charge is out of range!')
-    return False
-  elif charge_rate > 0.8:
-    print('Charge rate is out of range!')
-    return False
-
+  tempCheck(temperature)
+  socCheck(soc)
+  chargeCheck(charge_rate)
   return True
 
 
