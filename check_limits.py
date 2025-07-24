88f1992df6757ac2e2810f6f486ae880cdda2469
Dhanya91298 <Ramadas.Dhanya@in.bosch.com>
2025-07-24 10:13:49 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,18 +1,20 @@
+def minCheck(value, min)
+    return value < min
+def maxCheck:
+    return value > max
 def rangeCheck(value,min,max):
     retValmin = 1
     retValmax = 1
-    if min != -1:
-        if value < min:
-            retValmin = 0
-    if max != -1:
-        if value > max:
-            retValmax = 0
+    if min != 'NA':
+        retValmin = minCheck(value,min)
+    if max != 'NA':
+        retValmax = minCheck(value,max)
     return retValmin and retValmax
         
 def battery_is_ok(temperature, soc, charge_rate):
     tempVal = rangeCheck(temperature,0,45)
     socVal = rangeCheck(soc,20,80)
-    chargeVal = rangeCheck(charge_rate,-1,0.8)
+    chargeVal = rangeCheck(charge_rate,NA,0.8)
     
     print("Temperature out of range") if tempVal == 0 else None
     print("soc out of range") if socVal == 0  else None
