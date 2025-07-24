834007f7772b1098562bafe30db655319d4df889
Dhanya91298 <Ramadas.Dhanya@in.bosch.com>
2025-07-24 10:38:50 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -6,16 +6,19 @@ def rangeCheck(value, minv, maxv):
     return True
         
 def battery_is_ok(temperature, soc, charge_rate):
-    tempVal = rangeCheck(temperature,0,45)
-    socVal = rangeCheck(soc,20,80)
-    chargeVal = rangeCheck(charge_rate,'NA',0.8)
     
-    print("Temperature out of range") if tempVal == True else None
-    print("soc out of range") if socVal == True  else None
-    print("charge_rate out of range") if chargeVal == True else None
+    rangeChecksMessages = {
+        "Temperature ": rangeCheck(temperature, 0, 45),
+        "soc ": rangeCheck(soc, 20, 80),
+        "charge_rate ": rangeCheck(charge_rate, 'NA', 0.8)
+    }
+    
+    for label,checkResult in rangeChecksMessages.items():
+        if not checkResult:
+            print(label + "out of range")
     
     
-    return bool(tempVal and socVal and chargeVal)
+    return all(rangeChecksMessages.values())
 
 if __name__ == '__main__':
   assert(battery_is_ok(25, 70, 0.7) is True)
