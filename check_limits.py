f4504da480478319b8a017560c9f8a74abdc8bc9
Dhanya91298 <Ramadas.Dhanya@in.bosch.com>
2025-07-24 09:28:14 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -14,7 +14,7 @@ def chargeCheck(charge_rate):
       return False
 
 def battery_is_ok(temperature, soc, charge_rate):
-  return tempCheck(temperature) && socCheck(soc) && chargeCheck(charge_rate)
+  return tempCheck(temperature) and socCheck(soc) and chargeCheck(charge_rate)
 
 
 if __name__ == '__main__':
