dfcfd4a0e8a1ee6c4c66b57ca16c7a0672d64619
Dhanya91298 <Ramadas.Dhanya@in.bosch.com>
2025-07-24 10:28:18 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,28 +1,31 @@
-def minCheck(value, min)
-    return value < min
-def maxCheck:
-    return value > max
-def rangeCheck(value,min,max):
-    retValmin = 1
-    retValmax = 1
-    if min != 'NA':
-        retValmin = minCheck(value,min)
-    if max != 'NA':
-        retValmax = minCheck(value,max)
-    return retValmin and retValmax
+def minCheck(value, minv):
+    return bool(value >= minv)
+def maxCheck(value, maxv):
+    return bool(value <= maxv)
+def rangeCheck(value,minv,maxv):
+    retValmin = False
+    retValmax = False
+    if minv != 'NA':
+        retValmin = minCheck(value,minv)
+    else:
+        retValmin = True
+    if maxv != 'NA':
+        retValmax = maxCheck(value,maxv)
+    else:
+        retValmin = True
+    return bool(retValmin and retValmax)
         
 def battery_is_ok(temperature, soc, charge_rate):
     tempVal = rangeCheck(temperature,0,45)
     socVal = rangeCheck(soc,20,80)
-    chargeVal = rangeCheck(charge_rate,NA,0.8)
+    chargeVal = rangeCheck(charge_rate,'NA',0.8)
     
-    print("Temperature out of range") if tempVal == 0 else None
-    print("soc out of range") if socVal == 0  else None
-    print("charge_rate out of range") if chargeVal == 0 else None
+    print("Temperature out of range") if tempVal == True else None
+    print("soc out of range") if socVal == True  else None
+    print("charge_rate out of range") if chargeVal == True else None
     
     
     return bool(tempVal and socVal and chargeVal)
-    
 
 if __name__ == '__main__':
   assert(battery_is_ok(25, 70, 0.7) is True)
